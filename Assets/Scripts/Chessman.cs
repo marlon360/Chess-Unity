39f721ec91d0cfd190e44b90bcd61da39da678a3
Marlon Lückert <m.lueckert@me.com>
2019-05-23 14:20:31 +0200

    select/deselect chessman

--- Assets/Scripts/Chessman.cs
+++ Assets/Scripts/Chessman.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chessman : MonoBehaviour
-{
+public class Chessman : MonoBehaviour {
     public Vector2[] MoveDirections;
     public bool continuous;
     public bool allDirections;
     public bool jumping;
 
-    public void SetTile(Tile tile) {
-        transform.position = tile.transform.position + new Vector3(0, 2, 0);
+    private Rigidbody rigid;
+
+    void Start () {
+        rigid = GetComponent<Rigidbody> ();
+    }
+
+    public void SetTile (Tile tile) {
+        transform.position = tile.transform.position + new Vector3 (0, 2, 0);
         tile.chessman = this;
+    }
+
+    public void Select () {
+        rigid.useGravity = false;
+        transform.position = transform.position + new Vector3 (0, 2, 0);
+    }
+
+    public void Deselect () {
+        rigid.useGravity = true;
     }
 }
