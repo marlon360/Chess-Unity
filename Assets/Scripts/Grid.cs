925a16f006fba754d7dcd8e682cb1263c04f7fbf
Marlon Lückert <m.lueckert@me.com>
2019-05-23 13:57:00 +0200

    grid layout, tile highlight

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    public Tile BlackTile;
+    public Tile WhiteTile;
+
+    private Tile[,] tiles = new Tile[8,8];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupGrid();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void SetupGrid() {
+
+        BoxCollider blackCollider = BlackTile.GetComponent<BoxCollider>();
+        BoxCollider whiteCollider = WhiteTile.GetComponent<BoxCollider>();
+
+        Vector3 blackSize = blackCollider.bounds.extents;
+        Vector3 whiteSize = whiteCollider.bounds.extents;
+
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+        for (int u = 0; u < 8; u++) {
+            for (int v = 0; v < 8; v++) {
+                Tile nextTile = (u + v) % 2 == 0 ? BlackTile : WhiteTile;
+                GameObject tile = GameObject.Instantiate(nextTile.gameObject, new Vector3(v, 0, u), Quaternion.identity, gameObject.transform);
+                tiles[u, v] = nextTile;
+                string zAxis = (u+1).ToString();
+                string xAxis = alphabet[v].ToString();
+                tile.name = xAxis + zAxis;
+            }
+        }
+
     }
+
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Material HighlightMaterial;
+
+    private Renderer renderer;
+    private Material defaultMaterial;
+
+    void Stat() {
+        renderer = GetComponent<Renderer>();
+        defaultMaterial = renderer.material;
+    }
+
+    public void SelectTile() {
+        renderer.material = HighlightMaterial;
+    }
+
+    public void DeselectTile() {
+        renderer.material = defaultMaterial;
+    }
+
+}
