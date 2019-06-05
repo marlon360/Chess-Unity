6551283bb1e7b5297df388cd9245b0d9769e7367
Marlon Lückert <m.lueckert@me.com>
2019-06-05 12:07:10 +0200

    changed num of envs

--- trainLinux.sh
+++ trainLinux.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mlagents-learn config/trainer_config.yaml --env=dist/trainingLinux.x86_64 --no-graphics --num-envs=8 --run-id=$1 --train
+mlagents-learn config/trainer_config.yaml --env=dist/trainingLinux.x86_64 --no-graphics --num-envs=2 --run-id=$1 --train
